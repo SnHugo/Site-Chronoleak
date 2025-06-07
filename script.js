81dc94287fc0acbf376f1e30e12f3d4127e4f930
WesleyWaterkemper <oobuyuki@gmail.com>
2025-06-07 14:12:44 -0300

    feat: grid de imagens do desenvolvimento

--- script.js
+++ script.js
@@ -13,3 +13,22 @@
                 }
             });
         }
+        
+
+        // Expandir imagem da grid
+
+        const imagens = document.querySelectorAll('.grid-galeria img');
+        const modal = document.getElementById('modal');
+        const modalImagem = document.getElementById('modal-img');
+
+
+        imagens.forEach(img => {
+            img.addEventListener('click', () => {
+                modalImagem.src = img.src; 
+                modal.classList.add('ativo');
+            });
+        });
+
+        modal.addEventListener('click', () => {
+            modal.classList.remove('ativo');
+        });
